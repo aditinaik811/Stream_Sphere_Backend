21d8645b7c1a2eaded3527cb452e99746b245fa4
Aditi Naik <aditinaik811@gmail.com>
2025-08-30 21:31:13 +0530

    video update api created

--- routes/video.js
+++ routes/video.js
@@ -26,7 +26,7 @@ router.post('/upload-video',checkAuth,async(req,res)=>{
                 user_id:user._id,
                 videoUrl:uploadedVideo.secure_url,
                 videoId:uploadedVideo.public_id,
-                thumbnailUrl:uploadedVideo.secure_url,
+                thumbnailUrl:uploadedthumbnail.secure_url,
                 thumbnailId:uploadedthumbnail.public_id,
                 category:req.body.category,
                 tags:req.body.tags.split(","),
@@ -43,5 +43,63 @@ router.post('/upload-video',checkAuth,async(req,res)=>{
             error:err
         })
     }
+})
+
+
+router.put('/:videoId',checkAuth, async(req,res)=>{
+    try{
+        const verifiedUser = jwt.verify(req.headers.authorization.split(" ")[1],process.env.JWT_TOKEN)
+        const video = await Video.findById(req.params.videoId)
+        console.log(video)
+        if(video.user_id = verifiedUser._id){
+            if(req.files)
+            {
+                await cloudinary.uploader.destroy(video.thumbnailId)
+                const updatedThumbnail = await cloudinary.uploader.upload(req.files.thumbnail.tempFilePath)
+                const updatedData = {
+                    title:req.body.title,
+                    description:req.body.description,
+                    category:req.body.category,
+                    tags:req.body.tags.split(","),
+                    thumbnailUrl:updatedThumbnail.secure_url,
+                    thumbnailId:updatedThumbnail.public_id
+                }
+                const updatedVideoDetail = await Video.findByIdAndUpdate(req.params.videoId,updatedData,{new:true})
+                res.status(200).json({
+                    updatedVideo:updatedVideoDetail
+                })
+            }
+            else
+            {
+                    const updatedData = {
+                    title:req.body.title,
+                    description:req.body.description,
+                    category:req.body.category,
+                    tags:req.body.tags.split(",")
+                }
+                const updatedVideoDetail = await Video.findByIdAndUpdate(req.params.videoId,updatedData,{new:true})
+                res.status(200).json({
+                    updatedVideo:updatedVideoDetail
+                })
+            }
+        }
+        else
+        {
+            res.status(500).json({
+                error:"You have no permission"
+            })
+        }
+
+        
+
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    }
+
 })
 module.exports = router
